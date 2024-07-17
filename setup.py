fd79ea23f393d6b26279d2432a448b09a91ef85f
Yakir <yakir4123@gmail.com>
2024-07-18 01:06:48 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,13 @@ VERSION = '0.1.1'
 DESCRIPTION = 'Spiking Continues Time Neuron'
 LONG_DESCRIPTION = 'A Spiking Neural Network implementation using '
 
-with open("requirements.txt", "r", encoding="utf-8") as f:
-    REQUIRED_PACKAGES = f.read().splitlines()
+
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        lines = [line.strip() for line in lines if line and not line.startswith("#")]
+        return lines
+
 
 # Setting up
 setup(
@@ -16,11 +21,14 @@ setup(
     author_email="yakir4123@email.com>",
     description=DESCRIPTION,
     packages=find_packages(),
-    install_requires=REQUIRED_PACKAGES,
+    install_requires=parse_requirements('requirements.txt'),
     keywords=['python', 'snn', 'ai'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Education",
         "Programming Language :: Python :: 3",
+    ],
+    dependency_links=[
+        'git+https://github.com/BindsNET/bindsnet.git#egg=bindsnet'
     ]
 )
